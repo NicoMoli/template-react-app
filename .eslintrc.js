b11d4b0917b1ccde9e90f3ced794b7f1aea1d4de
Nicolás Molina <nmolina@teco.com.ar>
2021-12-04 11:22:35 -0300

    Initial commit, config eslint, prettier, husky

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["plugin:react/recommended", "standard", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+}
